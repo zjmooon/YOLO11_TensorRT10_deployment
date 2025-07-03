7db9d1d1f195aead77e60e39ec18e2a9832f26bd
zjmooon <48746140+zjmooon@users.noreply.github.com>
2025-07-03 18:05:43 +0800

    Update yolov11.h

--- src/yolov11.h
+++ src/yolov11.h
@@ -8,6 +8,40 @@ using namespace nvinfer1;
 using namespace std;
 using namespace cv;
 
+struct InferDeleter
+{
+    template <typename T>
+    void operator()(T* obj) const {
+        if (obj) delete obj;
+    } 
+};
+
+static auto StreamDeleter = [](cudaStream_t* pStream)
+{
+    if (pStream) {
+        static_cast<void>(cudaStreamDestroy(*pStream));
+        delete pStream;
+    }
+}
+
+struct BufferDeleter
+{
+    template <typename T>
+    void operator()(T* obj) const {
+        if (obj != nullptr) {
+            cudaFree(obj);
+            a = nullptr;
+        }
+    } 
+};
+struct InferDeleter
+{
+    template <typename T>
+    void operator()(T* obj) const {
+        if (obj) delete obj;
+    } 
+};
+
 struct Detection
 {
     float conf;
@@ -31,13 +65,24 @@ public:
 private:
     void init(std::string engine_path, nvinfer1::ILogger& logger);
 
-    float* gpu_buffers[2];               //!< The vector of device buffers needed for engine execution
-    float* cpu_output_buffer;
+    // float* gpu_buffers[2];               //!< The vector of device buffers needed for engine execution
+    // float* cpu_output_buffer;
 
-    cudaStream_t stream;
-    IRuntime* runtime;                 //!< The TensorRT runtime used to deserialize the engine
-    ICudaEngine* engine;               //!< The TensorRT engine used to run the network
-    IExecutionContext* context;        //!< The context for executing inference using an ICudaEngine
+    // GPU
+    std::unique_ptr<float, BufferDeleter> gpu_input_;
+    std::unique_ptr<float, BufferDeleter> gpu_output_;
+    std::vector<float>cpu_output;
+
+    // TensorRT
+    std::unique_ptr<nvinfer1::IRuntime, InferDeleter> runtime_;
+    std::unique_ptr<nvinfer1::ICudaEngine, InferDeleter> engine_;
+    std::unique_ptr<nvinfer1::IExecutionContext, InferDeleter> context_;
+    std::unique_ptr<cudaStream_t, decltype(StreamDeleter)> stream_(new cudaStream_t, StreamDeleter);
+
+    cudaStream_t stream = nullptr;
+    // IRuntime* runtime;                 //!< The TensorRT runtime used to deserialize the engine
+    // ICudaEngine* engine;               //!< The TensorRT engine used to run the network
+    // IExecutionContext* context;        //!< The context for executing inference using an ICudaEngine
 
     // Model parameters
     int input_w;
